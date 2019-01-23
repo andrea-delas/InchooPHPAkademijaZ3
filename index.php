397fea6f158be70faec118bec618777ea19190d9
andrea-delas <andrea.delas@outlook.com>
2019-01-23 18:29:56 +0100

    Kreiranje forme za unos broja redaka i stupaca

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zadaća - 2</title>
+</head>
+
+<body>
+<!-- forma za primanje brojeva putem POST parametra -->
+<form method = "POST">
+    <input type = "text" name = "numbers" placeholder="Broj redaka" />
+    <input type = "text" name = "numbers" placeholder="Broj stupaca" />
+
+    <input type = "submit" name = "submit" value="Pošalji" />
+</form>
+
+</body>
+
+</html>
+
+
+
 <?php
+
+$rowNumber = ($_POST['numbers']);
+$colNumber = ($_POST['numbers']);
+
 
 function spiral ($rowNumber, $colNumber, &$matrix)
 {
@@ -34,14 +63,13 @@ function spiral ($rowNumber, $colNumber, &$matrix)
     }
 }
 
-$rowNumber = 4;
-$colNumber = 6;
-spiral($rowNumber, $colNumber,$matrix);
-echo "<table>";
+
+spiral($rowNumber, $colNumber,$matrix);                                  //pozivanje funkcije
+echo "<table border =\"2\" style='border-collapse: collapse'; >";
 for ($i = 0; $i < $rowNumber; $i++) {
     echo "<tr>";
     for ($j = 0; $j < $colNumber; $j++) {
-        echo "<td>";
+        echo '<td style= "width: 20px; height: 20px;">';
         echo ($matrix[$i][$j]);
         echo "</td>";
     }
